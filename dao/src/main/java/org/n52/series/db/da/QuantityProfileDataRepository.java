c48499a0246df4c6190e2e16c113867ddabcc8b5
Henning Bredel <bredel@52north.org>
2017-05-09 15:02:14 +0200

    add profile repository

---
+++ dao/src/main/java/org/n52/series/db/da/QuantityProfileDataRepository.java
@@ -0,0 +1,142 @@
+
+package org.n52.series.db.da;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.hibernate.Session;
+import org.n52.io.response.dataset.profile.ProfileData;
+import org.n52.io.response.dataset.profile.ProfileValue;
+import org.n52.io.response.dataset.quantity.QuantityData;
+import org.n52.io.response.dataset.quantity.QuantityValue;
+import org.n52.series.db.DataAccessException;
+import org.n52.series.db.beans.DataEntity;
+import org.n52.series.db.beans.ProfileDataEntity;
+import org.n52.series.db.beans.ProfileDatasetEntity;
+import org.n52.series.db.beans.QuantityDataEntity;
+import org.n52.series.db.beans.QuantityDatasetEntity;
+import org.n52.series.db.da.QuantityProfileDataRepository.QuantityProfileDataItem;
+import org.n52.series.db.dao.DataDao;
+import org.n52.series.db.dao.DbQuery;
+
+public class QuantityProfileDataRepository
+        extends AbstractDataRepository<ProfileData, ProfileDatasetEntity, ProfileDataEntity, ProfileValue> {
+
+    private final QuantityDataRepository quantityRepository;
+
+    public QuantityProfileDataRepository() {
+        this.quantityRepository = new QuantityDataRepository();
+    }
+    
+    @Override
+    public ProfileValue getFirstValue(ProfileDatasetEntity dataset, Session session, DbQuery query) {
+        query.setComplexParent(true);
+        ProfileDataEntity parentEntity = super.getFirstValueEntity(dataset, query, session);
+        return assembleChildrenFrom(parentEntity, dataset, query);
+    }
+
+    @Override
+    public ProfileValue getLastValue(ProfileDatasetEntity dataset, Session session, DbQuery query) {
+        query.setComplexParent(true);
+        ProfileDataEntity parentEntity = super.getLastValueEntity(dataset, query, session);
+        return assembleChildrenFrom(parentEntity, dataset, query);
+    }
+
+    @Override
+    public Class<ProfileDatasetEntity> getDatasetEntityType() {
+        return ProfileDatasetEntity.class;
+    }
+    
+    private ProfileValue assembleChildrenFrom(ProfileDataEntity parentValue, ProfileDatasetEntity dataset, DbQuery query) {
+
+        ProfileValue profile = new ProfileValue();
+//        quantityRepository.getData(datasetId, dbQuery)
+        // TODO Auto-generated method stub
+        
+        List<QuantityProfileDataItem> dataItems = new ArrayList<>();
+        for (DataEntity< ? > dataEntity : parentValue.getValue()) {
+            QuantityDataEntity quantityEntity = (QuantityDataEntity) dataEntity;
+            QuantityValue valueItem = quantityRepository.createValue(quantityEntity.getValue(), quantityEntity, query);
+            addParameters(quantityEntity, valueItem, query);
+            Set<Map<String,Object>> parameters = valueItem.getParameters();
+            for (Map<String, Object> parameterObject : parameters) {
+                String verticalName = dataset.getVerticalParameterName();
+                if (isVertical(parameterObject, verticalName)) {
+                    // TODO vertical unit is missing for now
+                    QuantityProfileDataItem dataItem = new QuantityProfileDataItem();
+                    dataItem.setValue(quantityEntity.getValue());
+                    // set vertical's value
+                    dataItem.setVertical((double) parameterObject.get("value"));
+                    String verticalUnit = (String) parameterObject.get("unit");
+                    if (profile.getVerticalUnit() == null) {
+                        profile.setVerticalUnit(verticalUnit);
+                    }
+                    if (profile.getVerticalUnit() != null
+                            && profile.getVerticalUnit().equals(verticalUnit)) {
+                        dataItem.setVerticalUnit(verticalUnit);
+                    }
+                    dataItems.add(dataItem);
+                }
+            }
+        }
+        
+        profile.setValue(dataItems);
+        // TODO timestamp etc.
+        return profile;
+    }
+    
+    private boolean isVertical(Map<String, Object> parameterObject, String verticalName) {
+        return parameterObject.containsKey("name") 
+                && ((String)parameterObject.get("name")).equalsIgnoreCase(verticalName);
+    }
+
+    public static class QuantityProfileDataItem {
+        private String verticalUnit;
+        private Double vertical;
+        private Double value;
+        public String getVerticalUnit() {
+            return verticalUnit;
+        }
+        public void setVerticalUnit(String verticalUnit) {
+            this.verticalUnit = verticalUnit;
+        }
+        public Double getVertical() {
+            return vertical;
+        }
+        public void setVertical(Double vertical) {
+            this.vertical = vertical;
+        }
+        public Double getValue() {
+            return value;
+        }
+        public void setValue(Double value) {
+            this.value = value;
+        }
+        
+    }
+
+    @Override
+    protected ProfileValue createSeriesValueFor(ProfileDataEntity valueEntity,
+                                                ProfileDatasetEntity datasetEntity,
+                                                DbQuery query) {
+        return null;
+    }
+
+    @Override
+    protected ProfileData assembleData(ProfileDatasetEntity datasetEntity, DbQuery query, Session session)
+            throws DataAccessException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    protected ProfileData assembleDataWithReferenceValues(ProfileDatasetEntity datasetEntity,
+                                                          DbQuery dbQuery,
+                                                          Session session)
+            throws DataAccessException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}
