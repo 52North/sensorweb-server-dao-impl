3a7a2b4333af8a8d15bb1b41f5649dfe1405d183
Henning Bredel <h.bredel@52north.org>
2017-08-03 16:22:37 +0200

    differentiate when counting mobile/stationary platforms

--- dao/src/main/java/org/n52/series/db/dao/PlatformDao.java
+++ dao/src/main/java/org/n52/series/db/dao/PlatformDao.java
@@ -29,13 +29,18 @@
 
 package org.n52.series.db.dao;
 
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.criterion.DetachedCriteria;
 import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
+import org.hibernate.criterion.Subqueries;
+import org.n52.series.db.DataAccessException;
 import org.n52.series.db.beans.DatasetEntity;
 import org.n52.series.db.beans.DescribableEntity;
 import org.n52.series.db.beans.ObservationConstellationEntity;
 import org.n52.series.db.beans.PlatformEntity;
+import org.n52.series.db.beans.ProcedureEntity;
 import org.n52.series.db.beans.i18n.I18nPlatformEntity;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -44,6 +49,32 @@ public class PlatformDao extends ParameterDao<PlatformEntity, I18nPlatformEntity
 
     public PlatformDao(Session session) {
         super(session);
+    }
+
+    @Override
+    public Integer getCount(DbQuery query) throws DataAccessException {
+        DetachedCriteria stationary = QueryUtils.projectionOn("feature", createMobileSubquery("const", false));
+        DetachedCriteria mobile = QueryUtils.projectionOn("const.procedure", createMobileSubquery("const", true));
+
+        FeatureDao featureDao = new FeatureDao(session);
+        ProcedureDao procedureDao = new ProcedureDao(session);
+        return (int) Long.sum(count(stationary, featureDao, query),
+                              count(mobile, procedureDao, query));
+    }
+
+    private Long count(DetachedCriteria subquery, AbstractDao< ? > dao, DbQuery query) {
+        Criteria criteria = dao.getDefaultCriteria(query);
+        Criteria elements = criteria.add(Subqueries.propertyIn("pkid", subquery));
+        return (Long) elements.setProjection(Projections.rowCount())
+                              .uniqueResult();
+    }
+
+    private DetachedCriteria createMobileSubquery(String constellationAlias, boolean mobile) {
+        DetachedCriteria criteria = DetachedCriteria.forClass(DatasetEntity.class);
+        criteria.createCriteria(DatasetEntity.PROPERTY_OBSERVATION_CONSTELLATION, constellationAlias)
+                .createCriteria(ObservationConstellationEntity.PROCEDURE)
+                .add(Restrictions.eq(ProcedureEntity.PROPERTY_MOBILE, mobile));
+        return criteria;
     }
 
     @Override
