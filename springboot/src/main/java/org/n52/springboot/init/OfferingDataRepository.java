383991831f280525ca1ced9d13df113fcadda8e1
Henning Bredel <bredel@52north.org>
2018-05-18 06:24:16 +0200

    Avoid name clashes of other bean named OfferingRepository

--- springboot/src/main/java/org/n52/springboot/init/OfferingDataRepository.java
+++ springboot/src/main/java/org/n52/springboot/init/OfferingDataRepository.java
@@ -3,6 +3,6 @@ package org.n52.springboot.init;
 import org.n52.series.db.beans.OfferingEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OfferingRepository extends JpaRepository<OfferingEntity, Long> {
+public interface OfferingDataRepository extends JpaRepository<OfferingEntity, Long> {
 
 }
