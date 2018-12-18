10189c043280f4b803b100a5a9258a9e664944c7
Christian Autermann <c.autermann@52north.org>
2018-12-18 14:12:32 +0100

    mark DataRepositoryComponent as Inherited

--- dao/src/main/java/org/n52/series/db/DataRepositoryComponent.java
+++ dao/src/main/java/org/n52/series/db/DataRepositoryComponent.java
@@ -2,17 +2,20 @@ package org.n52.series.db;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import org.n52.series.db.beans.DatasetEntity;
 import org.springframework.stereotype.Component;
 
-@Target({ElementType.TYPE})
+import org.n52.series.db.beans.DatasetEntity;
+
+@Target({ ElementType.TYPE })
 @Retention(RetentionPolicy.RUNTIME)
 @Component
 @Documented
+@Inherited
 public @interface DataRepositoryComponent {
 
     /**
