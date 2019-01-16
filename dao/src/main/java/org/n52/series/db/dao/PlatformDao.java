7f45aeaccafc3e4f0c5f2dc7d7f8f6a8965fbfe6
Carsten Hollmann <c.hollmann@52north.org>
2019-01-16 15:12:10 +0100

    Remove obsolete overwritten methods for count getCount and addSpatialFilter

--- dao/src/main/java/org/n52/series/db/dao/PlatformDao.java
+++ dao/src/main/java/org/n52/series/db/dao/PlatformDao.java
@@ -30,14 +30,8 @@ package org.n52.series.db.dao;
 
 import org.hibernate.Criteria;
 import org.hibernate.Session;
-import org.hibernate.criterion.DetachedCriteria;
 import org.hibernate.criterion.Projections;
-import org.hibernate.criterion.Subqueries;
-import org.n52.io.request.FilterResolver;
-import org.n52.series.db.DataAccessException;
-import org.n52.series.db.beans.DataEntity;
 import org.n52.series.db.beans.DatasetEntity;
-import org.n52.series.db.beans.IdEntity;
 import org.n52.series.db.beans.PlatformEntity;
 import org.n52.series.db.beans.i18n.I18nPlatformEntity;
 import org.springframework.transaction.annotation.Transactional;
@@ -49,34 +43,10 @@ public class PlatformDao extends ParameterDao<PlatformEntity, I18nPlatformEntity
         super(session);
     }
 
-    @Override
-    public Integer getCount(DbQuery query) throws DataAccessException {
-        // DetachedCriteria mobile =
-        // QueryUtils.projectionOn(DatasetEntity.PROPERTY_PROCEDURE,
-        // createMobileFilter(true))
-        // .setResultTransformer(DistinctResultTransformer.INSTANCE);
-        // DetachedCriteria stationary =
-        // QueryUtils.projectionOn(DatasetEntity.PROPERTY_FEATURE,
-        // createMobileFilter(false))
-        // .setResultTransformer(DistinctResultTransformer.INSTANCE);
-
-        DatasetDao<DatasetEntity> datasetDao = new DatasetDao<>(session);
-        return count(datasetDao, query).intValue();
-    }
-
     private Long count(AbstractDao<?> dao, DbQuery query) {
         Criteria criteria = dao.getDefaultCriteria(query);
-        // Criteria elements =
-        // criteria.add(Subqueries.propertyIn(IdEntity.PROPERTY_ID, subquery));
         return (Long) criteria.setProjection(Projections.rowCount()).uniqueResult();
     }
-
-    // private DetachedCriteria createMobileFilter(boolean mobile) {
-    // DetachedCriteria criteria =
-    // DetachedCriteria.forClass(DatasetEntity.class);
-    // criteria.add(Restrictions.eq(DatasetEntity.PROPERTY_MOBILE, mobile));
-    // return criteria;
-    // }
 
     @Override
     protected String getDatasetProperty() {
@@ -91,44 +61,6 @@ public class PlatformDao extends ParameterDao<PlatformEntity, I18nPlatformEntity
     @Override
     protected Class<I18nPlatformEntity> getI18NEntityClass() {
         return I18nPlatformEntity.class;
-    }
-
-    @Override
-    protected DetachedCriteria addSpatialFilter(DbQuery query, DetachedCriteria criteria) {
-        /*
-         * Filters for the last known location (max result time) of a mobile
-         * platform
-         *
-         * Here we do have to consider mobile variants only (mobile=true has
-         * been set beforehand) as repository decides already which DAO is used
-         * to query stationary (--> FeatureDao) and mobile platforms
-         */
-        FilterResolver filterResolver = query.getFilterResolver();
-        if (filterResolver.isSetMobileFilter()) {
-
-            // values for oldest result time
-            String rtAlias = "rtAlias";
-            // String rtColumn = QueryUtils.createAssociation(rtAlias, column);
-            String rtDatasetId = QueryUtils.createAssociation(rtAlias, IdEntity.PROPERTY_ID);
-            String rtResultTime = QueryUtils.createAssociation(rtAlias, DataEntity.PROPERTY_RESULT_TIME);
-
-            DetachedCriteria maxResultTimeByDatasetId = DetachedCriteria.forClass(DataEntity.class, rtAlias);
-            maxResultTimeByDatasetId.setProjection(Projections.projectionList()
-                    // .add(Projections.groupProperty(rtColumn))
-                    .add(Projections.groupProperty(rtDatasetId)).add(Projections.max(rtResultTime)));
-
-            String[] matchProperties = new String[] { IdEntity.PROPERTY_ID,
-                    // DataEntity.PROPERTY_SERIES_PKID,
-                    DataEntity.PROPERTY_RESULT_TIME };
-            DetachedCriteria observationCriteria = query.addSpatialFilter(DetachedCriteria.forClass(DataEntity.class))
-                    .add(Subqueries.propertiesIn(matchProperties, maxResultTimeByDatasetId))
-                    .createCriteria(DataEntity.PROPERTY_DATASET)
-                    .setProjection(Projections.property(DataEntity.PROPERTY_ID));
-
-            criteria.add(Subqueries.propertyIn(IdEntity.PROPERTY_ID, observationCriteria));
-        }
-
-        return criteria;
     }
 
 }
