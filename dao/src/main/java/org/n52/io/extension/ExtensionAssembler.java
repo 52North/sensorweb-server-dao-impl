00ef59df1c8712d69026ea23d0ba09b77537a360
Carsten Hollmann <c.hollmann@52north.org>
2019-08-16 12:20:49 +0200

    add abstract extension assembler

---
+++ dao/src/main/java/org/n52/io/extension/ExtensionAssembler.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2015-2019 52°North Initiative for Geospatial Open Source
+ * Software GmbH
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation.
+ *
+ * If the program is linked with libraries which are licensed under one of
+ * the following licenses, the combination of the program with the linked
+ * library is not considered a "derivative work" of the program:
+ *
+ *     - Apache License, version 2.0
+ *     - Apache Software License, version 1.0
+ *     - GNU Lesser General Public License, version 3
+ *     - Mozilla Public License, versions 1.0, 1.1 and 2.0
+ *     - Common Development and Distribution License (CDDL), version 1.0
+ *
+ * Therefore the distribution of the program linked with libraries licensed
+ * under the aforementioned licenses, is permitted by the copyright holders
+ * if the distribution is compliant with both the GNU General Public License
+ * version 2 and the aforementioned licenses.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ */
+package org.n52.io.extension;
+
+import org.n52.io.request.IoParameters;
+import org.n52.series.db.old.dao.DbQuery;
+import org.n52.series.db.old.dao.DbQueryFactory;
+import org.n52.series.db.repositories.core.DatasetRepository;
+
+public abstract class ExtensionAssembler {
+
+    private DbQueryFactory dbQueryFactory;
+    private DatasetRepository datasetRepository;
+
+    public ExtensionAssembler(DatasetRepository datasetRepository, DbQueryFactory dbQueryFactory) {
+        this.datasetRepository = datasetRepository;
+        this.dbQueryFactory = dbQueryFactory;
+    }
+
+    public DatasetRepository getDatasetRepository() {
+        return datasetRepository;
+    }
+
+    public DbQuery getDbQuery(IoParameters parameters) {
+        return dbQueryFactory.createFrom(parameters);
+    }
+}
