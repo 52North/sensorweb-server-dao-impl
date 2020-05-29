be80dc7819914449511b7ecabad8b0d0eee4e2f3
Carsten Hollmann <c.hollmann@52north.org>
2020-05-29 13:56:58 +0200

    add getLevel

--- dao/src/main/java/org/n52/series/db/dao/DbQuery.java
+++ dao/src/main/java/org/n52/series/db/dao/DbQuery.java
@@ -149,6 +149,10 @@ public class DbQuery {
                          .toInterval();
     }
 
+    public Integer getLevel() {
+        return parameters.getLevel();
+    }
+
     public Envelope getSpatialFilter() {
         BoundingBox spatialFilter = parameters.getSpatialFilter();
         if (spatialFilter != null) {
