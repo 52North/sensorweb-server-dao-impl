0f1ec7cbc8b32480641dc1d47db3e3783271eab9
Carsten Hollmann <c.hollmann@52north.org>
2021-04-29 16:10:36 +0200

    make methods public

--- dao/src/main/java/org/n52/series/db/da/AbstractDataRepository.java
+++ dao/src/main/java/org/n52/series/db/da/AbstractDataRepository.java
@@ -272,7 +272,7 @@ public abstract class AbstractDataRepository<S extends DatasetEntity,
         return null;
     }
 
-    protected Long getCount(DatasetEntity dataset, DbQuery query, Session session) {
+    public Long getCount(DatasetEntity dataset, DbQuery query, Session session) {
         return createDataDao(session).getCount(dataset);
     }
 }
--- dao/src/main/java/org/n52/series/db/da/AbstractNumericalDataRepository.java
+++ dao/src/main/java/org/n52/series/db/da/AbstractNumericalDataRepository.java
@@ -39,15 +39,15 @@ import org.n52.series.db.dao.DbQuery;
 public abstract class AbstractNumericalDataRepository<E extends DataEntity<T>, V extends AbstractValue<?>, T>
         extends AbstractDataRepository<DatasetEntity, E, V, T> {
 
-    protected V getMax(DatasetEntity dataset, DbQuery query, Session session) {
+    public V getMax(DatasetEntity dataset, DbQuery query, Session session) {
         return assembleDataValue(createDataDao(session).getMax(dataset), dataset, query);
     }
 
-    protected V getMin(DatasetEntity dataset, DbQuery query, Session session) {
+    public V getMin(DatasetEntity dataset, DbQuery query, Session session) {
         return assembleDataValue(createDataDao(session).getMin(dataset), dataset, query);
     }
 
-    protected BigDecimal getAverage(DatasetEntity dataset, DbQuery query, Session session) {
+    public BigDecimal getAverage(DatasetEntity dataset, DbQuery query, Session session) {
         return createDataDao(session).getAvg(dataset);
     }
 
