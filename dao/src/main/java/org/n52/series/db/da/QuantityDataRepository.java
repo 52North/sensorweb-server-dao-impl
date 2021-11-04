306249efa0858dc1d06d2e3a7904e8a39c43ccd7
Carsten Hollmann <c.hollmann@52north.org>
2021-11-04 08:47:32 +0100

    Define QuantityDataEntity for querying observaiton in QuantityDataRepository (reduces the returned columns)

--- dao/src/main/java/org/n52/series/db/da/QuantityDataRepository.java
+++ dao/src/main/java/org/n52/series/db/da/QuantityDataRepository.java
@@ -180,7 +180,7 @@ public class QuantityDataRepository
                 }
             }
         }
-        DataDao obsDao = new DataDao(session);
+        DataDao obsDao = createDataDao(session);
         List<DataEntity> observations = obsDao.getAllInstancesFor(series, dbQuery);
         for (DataEntity dataEntity : observations) {
             QuantityDataEntity observationEntity = unproxy(dataEntity, session);
@@ -347,5 +347,10 @@ public class QuantityDataRepository
 
     private BigDecimal format(QuantityDataEntity observation, DatasetEntity dataset) {
         return format(observation.getValue(), dataset);
+    }
+
+    @Override
+    protected DataDao<QuantityDataEntity> createDataDao(Session session) {
+        return new DataDao(session, QuantityDataEntity.class);
     }
 }
